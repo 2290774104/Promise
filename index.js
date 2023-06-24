67b52c594bb4f7c74f6923c0f760174dfef6f5c2
2290774107@qq.com <2290774104@qq.com>
2023-06-24 16:34:52 +0800

    feat: 实现Promise

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+const Promise = require("./Promise.js");
+
+function readFile(url) {
+  let dfd = Promise.deferred();
+
+  fs.readFile(url, "utf-8", function (err, data) {
+    if (err) return dfd.reject(err);
+    dfd.resolve(data);
+  });
+
+  return dfd.promise;
+}
+
+readFile(path.resolve(__dirname, "name.txt")).then((data) => {
+  console.log(data);
+});
